1aa3dc75ded9c52310688afd2219d14023cb327a
Muhammad Talha <135458367+MTalhaofc@users.noreply.github.com>
2024-06-14 18:56:47 +0500

    Add files via upload

---
+++ Task5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/ClassWork/css/bootstrap.css">
+</head>
+<body>
+    <div class="row">
+    <div  class="col-md-8" style="background-color:black ; color:aqua;"> hi</div>
+    <div class="col-md-4" style="background-color:red ; color:aqua;"> hi</div>
+    <?php 
+    echo MyFirstApp
+    ?>
+</div>
+</body>
+</html>
